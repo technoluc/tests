d62f4106fb7e8f3355f783ecccac96c75eb1f582
Luc Kurstjens <luc@technoluc.nl>
2022-05-25 17:20:31 +0200

    test brackets

--- testpath.ps1
+++ testpath.ps1
@@ -18,26 +18,30 @@ else {
 
 if ($choice -eq '1') {
 
-    if (-not(Test-Path -Path $pcatemptyoutput -PathType Leaf)) {
+    if (-not(Test-Path -Path $pcatemptyoutput -PathType Leaf)) 
+        {
             Write-Host "Downloading pop-cat Empty Icon"
             $url = $pcatempty
             Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pcatemptyoutput" -Force )
         }
-        else {
+        else 
+        {
             Write-Host "Pop-cat Empty Icon already exists"
         }
     }
 
-    if (-not(Test-Path -Path $pcatfulloutput -PathType Leaf)) {
+    if (-not(Test-Path -Path $pcatfulloutput -PathType Leaf)) 
+        {
             Write-Host "Downloading pop-cat Full Icon"
             $url = $pcatfull
             Invoke-WebRequest -Uri $url -OutFile ( New-Item -Path "$pcatfulloutput" -Force )
         }
-        else {
+        else 
+        {
             Write-Host "Pop-cat Full Icon already exists"
         }
     }
-
+}
 
     Write-Host "Setting Registry"
     Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CLSID\{645FF040-5081-101B-9F08-00AA002F954E}\DefaultIcon" -Name "(Default)" -Value "$env:userprofile\icons\pop-cat-empty.ico,0"
